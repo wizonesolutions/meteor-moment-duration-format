37bdf844e3cab894ea921d756d051f44e1431887
Kevin Kaland <kevin@wizonesolutions.com>
2014-09-01 09:59:14 +0200

    Upgrade to Meteor 0.9.0.
    This sees a re-versioning; since SemVer doesn't play too well with matching upstream version packages, I'm
    just going to use my own version identifiers now.

--- package.js
+++ package.js
@@ -1,15 +1,18 @@
 Package.describe({
-  summary: 'Format Moment Duration objects.'
+  summary: 'Format Moment Duration objects.',
+  version: "1.3.0",
+  git: "https://github.com/wizonesolutions/meteor-moment-duration-format.git"
 });
 
 Package.on_use(function (api, where) {
+  api.versionsFrom("METEOR@0.9.0");
   where = where || ['client', 'server'];
   api.use("underscore", where);
-  api.use("moment", where);
+  api.use("mrt:moment@2.6.0", where);
 
   if (api.imply) {
     api.imply("underscore", where);
-    api.imply("moment", where);
+    api.imply("mrt:moment", where);
   }
 
   api.add_files(['compatibility.js', 'lib/moment-duration-format/lib/moment-duration-format.js'], where);
